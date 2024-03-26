ce74c4730fbd585c98b777bdf5886a4d66768fcb
Sebastian Gadzinski <45364086+SebGadzinski@users.noreply.github.com>
2024-03-26 19:26:02 -0400

    forgot

--- src/scripts/updateClient.js
+++ src/scripts/updateClient.js
@@ -22,17 +22,6 @@ try {
     execSync('npm run build-www', { cwd: projectDir }); // Assuming 'npm run build' is the correct command
     console.log('Project build completed.');
 
-    // Remove the versioning
-    execSync('git tag -l | xargs git tag -d', { cwd: projectDir });
-    execSync('git stash --include-untracked', { cwd: projectDir });
-    execSync('git reset HEAD~1', { cwd: projectDir });
-    execSync('git stash pop --index 0', { cwd: projectDir });
-    execSync('git restore package.json', { cwd: projectDir });
-    execSync('git restore package-lock.json', { cwd: projectDir });
-    execSync('git restore src-capacitor/package.json', { cwd: projectDir });
-    execSync('git restore src-capacitor/package-lock.json', { cwd: projectDir });
-    console.log('Cleared Versioning.');
-
     // Assuming you want to move files from a directory named ClientUpdater inside projectDir to a directory named Client
     const sourceDir = path.resolve(projectDir, 'src-capacitor', 'www');
     // Define the destination directory (two levels up from projectDir, then into 'Client')
