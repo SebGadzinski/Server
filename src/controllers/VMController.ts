f019505351a134cef6adcb8c2b02945206f24cd3
Sebastian Gadzinski <45364086+SebGadzinski@users.noreply.github.com>
2024-03-26 19:51:22 -0400

    update?

--- src/controllers/VMController.ts
+++ src/controllers/VMController.ts
@@ -9,21 +9,21 @@ import Result from '../classes/Result';
 import { Config } from '../models';
 
 class VMController {
-    public gitPull(req: any, res: any) {
-        res.send(new Result({ success: true }));
-
-        const projectDir = path.resolve(__dirname, '..');
-        const scriptPath = path.resolve(projectDir, 'scripts', 'gitPull.js');
-
-        // Spawn the update and seed script in a detached process
-        const updateProcess = spawn('node', [scriptPath], { detached: true, stdio: 'ignore' });
-        updateProcess.unref();
-    }
     public reseedDB(req: any, res: any) {
         res.send(new Result({ success: true }));
 
         const projectDir = path.resolve(__dirname, '..');
         const scriptPath = path.resolve(projectDir, 'scripts', 'reseedDB.js');
+
+        // Spawn the update and seed script in a detached process
+        const updateProcess = spawn('node', [scriptPath], { detached: true, stdio: 'ignore' });
+        updateProcess.unref();
+    }
+    public gitPull(req: any, res: any) {
+        res.send(new Result({ success: true }));
+
+        const projectDir = path.resolve(__dirname, '..');
+        const scriptPath = path.resolve(projectDir, 'scripts', 'gitPull.js');
 
         // Spawn the update and seed script in a detached process
         const updateProcess = spawn('node', [scriptPath], { detached: true, stdio: 'ignore' });
@@ -35,7 +35,18 @@ class VMController {
             const scriptPath = path.resolve(projectDir, 'scripts', 'updateClient.js');
 
             // TODO: Causing crash
-            execSync(`node ${scriptPath}`);
+            const updateProcess = spawn('node', [scriptPath], { detached: true, stdio: 'inherit' });
+            updateProcess.unref();
+            updateProcess.on('error', (err) => {
+                console.error('Failed to start subprocess.', err);
+            });
+            updateProcess.stdout.on('data', (data) => {
+                console.log(`stdout: ${data}`);
+            });
+
+            updateProcess.stderr.on('data', (data) => {
+                console.error(`stderr: ${data}`);
+            });
         } catch (err) {
             console.log(err);
         }
