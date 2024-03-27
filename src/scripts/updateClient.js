b2bcf7f2423979d1144ba80c59aaebc2b56b7aa0
Sebastian Gadzinski <45364086+SebGadzinski@users.noreply.github.com>
2024-03-27 08:51:15 -0400

    client update mabye

--- src/scripts/updateClient.js
+++ src/scripts/updateClient.js
@@ -18,25 +18,9 @@ try {
     execSync('/usr/bin/git pull', { cwd: projectDir, stdio: 'inherit', shell: true });
     console.log('Git pull completed.');
 
-    // Build the project
-    execSync('npm run build-www', { cwd: projectDir }); // Assuming 'npm run build' is the correct command
-    console.log('Project build completed.');
-
-    // Assuming you want to move files from a directory named ClientUpdater inside projectDir to a directory named Client
-    const sourceDir = path.resolve(projectDir, 'src-capacitor', 'www');
-    // Define the destination directory (two levels up from projectDir, then into 'Client')
-    const destinationDir = path.resolve(projectDir, '..', '..', 'Client');
-
-    // Copy the files from sourceDir to destinationDir
-    try {
-        console.log(`Copying: ${sourceDir}`);
-        console.log(`Moving To: ${destinationDir}`);
-        execSync(`cp -Rf "${sourceDir}/." "${destinationDir}/"`, { cwd: projectDir });
-        console.log('Files have been copied.');
-    } catch (error) {
-        console.error('An error occurred while copying files:', error);
-    }
-
+    // Pull latest changes from Git
+    execSync('sudo ./prod-install.sh', { cwd: projectDir, stdio: 'inherit', shell: true });
+    console.log('Client updated.');
 } catch (error) {
     console.error('An error occurred:', error);
 }
